f9d56e8535e6f4ac04707aaf31265722d60c66d1
Mooje Kong <mooje.kong@gmail.com>
2020-05-13 18:16:53 +0900

    add IMG_VERSION to start using specific container images and modify tls information in store1

--- app/store1/adduser.js
+++ app/store1/adduser.js
@@ -17,7 +17,7 @@ async function main() {
         // Identity to credentials to be stored in the wallet
         const credPath = path.join(fixtures, '/crypto-config/peerOrganizations/store1.mymarket.com/users/User1@store1.mymarket.com');
         const cert = fs.readFileSync(path.join(credPath, '/msp/signcerts/User1@store1.mymarket.com-cert.pem')).toString();
-        const key = fs.readFileSync(path.join(credPath, '/msp/keystore/c490fdddf911d54f6fd771fc7c8b5451fa3c96707af4c2e8fd77ee3f5c75c6dc_sk')).toString();
+        const key = fs.readFileSync(path.join(credPath, '/msp/keystore/4a373b84fbd315617e10929db6eed5409f43173438224bf843c30ad17b3d3fae_sk')).toString();
 
         // Load credentials into wallet
         const identityLabel = 'User1@store1.mymarket.com';
--- stop.sh
+++ stop.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 export COMPOSE_PROJECT_NAME=mymarket
+export IMG_VERSION=1.4.6
 
 docker-compose down
